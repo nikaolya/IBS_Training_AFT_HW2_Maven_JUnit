e6768742984f048458190e82d6ac29111946c3f1
Dominika-Olga Volgina <dominica_volgina@mail.ru>
2022-09-29 12:14:15 +0300

    Initial commit

---
+++ src/test/java/tests/ExampleTest.java
@@ -0,0 +1,12 @@
+package tests;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ExampleTest {
+	@Test
+	public void test1(){
+		Assert.assertTrue(true);
+		System.out.println();
+	}
+}
